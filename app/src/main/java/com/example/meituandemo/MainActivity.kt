187ab7c1eb504d25456c563c18aa27c116409435
SP6 <qwer14788lll@163.com>
2020-10-19 01:41:42 +0800

    改善了文字显示效果

--- app/src/main/java/com/example/meituandemo/MainActivity.kt
+++ app/src/main/java/com/example/meituandemo/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.meituandemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.ArrayList
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +13,71 @@ class MainActivity : AppCompatActivity() {
     private fun initHomeData() {
         repeat(8)
         {
-            homeDataList.add(HomeData(R.drawable.home_01,"我和我的家乡","葛优,黄渤,范伟","9.3分 | 442858人观看","￥19.9","起","退","改签"))
-            homeDataList.add(HomeData(R.drawable.home_02,"数码宝贝：最后的进化","数码宝贝系列动画，历经20年的成长","88063人想看","即将上映"))
-            homeDataList.add(HomeData(R.drawable.home_03,"淼福猪肚鸡（上社牌坊店）","服务真的很好呢","4.5分 | 513个评价","￥94.8","￥158.0","6折"))
-            homeDataList.add(HomeData(R.drawable.home_04,"82号古宅","葛天扈,天翼","暂无评分","￥14.9","起","改签"))
-            homeDataList.add(HomeData(R.drawable.home_05,"探蟹肉蟹煲（百脑汇店）","太好吃太划算了","4.1分","￥118","155","已减37元"))
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_01,
+                    "我和我的家乡",
+                    "葛优,黄渤,范伟",
+                    "9.3分 | 442858人观看",
+                    "￥19.9",
+                    "起",
+                    "退",
+                    "改签"
+                )
+            )
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_02,
+                    "数码宝贝：最后的进化",
+                    "数码宝贝系列动画，历经20年的成长",
+                    "88063人想看",
+                    "即将上映"
+                )
+            )
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_03,
+                    "淼福猪肚鸡",
+                    "服务真的很好呢",
+                    "4.5分 | 513个评价",
+                    "￥94.8",
+                    "￥158.0",
+                    "6折"
+                )
+            )
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_04,
+                    "82号古宅",
+                    "葛天扈,天翼",
+                    "暂无评分",
+                    "￥14.9",
+                    "起",
+                    "改签"
+                )
+            )
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_05,
+                    "探蟹肉蟹煲",
+                    "太好吃太划算了",
+                    "4.1分",
+                    "￥118",
+                    "155",
+                    "已减37元"
+                )
+            )
+            homeDataList.add(
+                HomeData(
+                    R.drawable.home_06,
+                    "嘆牛自助放题 澳洲牛肉自主放题",
+                    "我觉得真的很好吃",
+                    "半年售5000+ | 9.7km",
+                    "￥108",
+                    "138",
+                    "已减30元"
+                )
+            )
         }
     }
 
